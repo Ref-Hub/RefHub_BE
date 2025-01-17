8185b1d5b80fecad31c4aeb5c337cdf397da8970
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-01-18 00:35:21 +0900

    Chore: config.js 환경변수 설정 추가

---
+++ config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+require("dotenv").config();
+
+module.exports = {
+  mailer: {
+    gmailUser: process.env.GMAIL_OAUTH_USER,
+    gmailClientId: process.env.GMAIL_OAUTH_CLIENT_ID,
+    gmailClientSecret: process.env.GAMIL_OAUTH_CLIENT_SECRET,
+    gmailRefreshToken: process.env.GAMIL_OAUTH_REFRESH_TOKEN,
+  },
+};
