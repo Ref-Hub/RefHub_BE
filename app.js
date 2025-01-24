22b9faa287cee34bcd459973dbcb3942e5e1976d
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-01-25 02:06:42 +0900

    Refactor: 에러 메시지 변경

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-"use strict";
-
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/connect.js";
+import connectDB from "./db.js"
 import { StatusCodes } from "http-status-codes";
 import cors from "cors";
-import Collection from "./models/Collection.js";
-import User from "./models/User.js";
 import collectionRoutes from "./routes/collectionRoutes.js";
+import jwt from "jsonwebtoken";
 
 // .env 설정
 dotenv.config();
@@ -17,12 +14,26 @@ app.use(cors());
 app.use(express.json());
 connectDB();
 
+// // Token Test Code
+
+// const payload = {
+//   id: "67851e7cd00c4a5843c88303",
+//   email: "yyj9694651@gmail.com",
+// };
+// const secret = process.env.JWT_SECRET;
+// const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+// console.log(token);
+
+// if (!token) {
+//   res.status(401).send({ error: "No token, authorization denied" });
+// }
+
 // 에러 처리 미들웨어
 const errorHandler = (err, req, res, next) => {
   console.error("Error: ", err);
   res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: "서버 오류가 발생했습니다." });
+    .json({ error: "서버 오류가 발생했습니다." });
 };
 
 // app.use("/users", userRoutes);
@@ -31,8 +42,12 @@ app.use("/collections", collectionRoutes);
 
 // 테스트용 API
 app.get("/", async (req, res, next) => {
-  console.log("hello world!");
-  res.status(StatusCodes.OK).json({ message: "접속 성공" });
+  try {
+    console.log("hello world!");
+    res.status(StatusCodes.OK).json({ message: "접속 성공" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.use(errorHandler);
