06c415f6fd3a197234b30f3c0f7a624445e11024
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-01-25 23:32:36 +0900

    Feat: 전체 에러 처리 미들웨어 및 주석 처리된 토큰 생성 코드 추가

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./db.js"
-import { StatusCodes } from "http-status-codes";
 import cors from "cors";
-import collectionRoutes from "./routes/collectionRoutes.js";
-import jwt from "jsonwebtoken";
+import connectDB from "./db.js";
+// import jwt from "jsonwebtoken";
 
-// .env 설정
-dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
+import collectionRoutes from "./routes/collectionRoutes.js";
+import referenceRoutes from "./routes/referenceRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB 연결
 connectDB();
 
-// // Token Test Code
+/*
+// Token Test Code
 
-// const payload = {
-//   id: "67851e7cd00c4a5843c88303",
-//   email: "yyj9694651@gmail.com",
-// };
-// const secret = process.env.JWT_SECRET;
-// const token = jwt.sign(payload, secret, { expiresIn: "1h" });
-// console.log(token);
+const payload = {
+  id: "67851e7cd00c4a5843c88303",
+  email: "yyj9694651@gmail.com",
+};
+const secret = process.env.JWT_SECRET;
+const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+console.log(token);
 
-// if (!token) {
-//   res.status(401).send({ error: "No token, authorization denied" });
-// }
+if (!token) {
+  res.status(401).send({ error: "No token, authorization denied" });
+}
+  */
 
 // 에러 처리 미들웨어
 const errorHandler = (err, req, res, next) => {
@@ -36,19 +39,10 @@ const errorHandler = (err, req, res, next) => {
     .json({ error: "서버 오류가 발생했습니다." });
 };
 
-// app.use("/users", userRoutes);
-// app.use("/collections/references", referenceRoutes);
-app.use("/collections", collectionRoutes);
-
-// 테스트용 API
-app.get("/", async (req, res, next) => {
-  try {
-    console.log("hello world!");
-    res.status(StatusCodes.OK).json({ message: "접속 성공" });
-  } catch (err) {
-    next(err);
-  }
-});
+// 라우트 설정
+app.use("/api/users", userRoutes);
+app.use("/api/collections", collectionRoutes);
+app.use("/api/reference", referenceRoutes);
 
 app.use(errorHandler);
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
