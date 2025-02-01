d90d20ac88b8ddc0c933952455b9becfc337b45d
SONG <dlthdal07@gmail.com>
2025-02-01 22:33:59 +0900

    Fix: credentials값 true로 변경

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors({
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: false,
+  credentials: true,
   maxAge: 86400
 }));
 app.use(express.json());
