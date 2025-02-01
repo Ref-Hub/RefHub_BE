35975e6720e29eef4448cbc646c3d23f9f43df62
SONG <dlthdal07@gmail.com>
2025-02-01 22:05:56 +0900

    Fix: CORS 설정 변경

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./db.js";
-//import jwt from "jsonwebtoken";
 
 import userRoutes from "./routes/userRoutes.js";
 import collectionRoutes from "./routes/collectionRoutes.js";
@@ -15,23 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 // DB 연결
 connectDB();
 
-/*
-// Token Test Code
-
-
-const payload = {
-  id: "67851e7cd00c4a5843c88303",
-  email: "yyj9694651@gmail.com",
-};
-const secret = process.env.JWT_SECRET;
-const token = jwt.sign(payload, secret, { expiresIn: "1h" });
-console.log(token);
-
-if (!token) {
-  res.status(401).send({ error: "No token, authorization denied" });
-}
-*/
-
 // 에러 처리 미들웨어
 const errorHandler = (err, req, res, next) => {
   console.error("Error: ", err);
@@ -44,6 +26,18 @@ const errorHandler = (err, req, res, next) => {
 app.use("/api/users", userRoutes);
 app.use("/api/collections", collectionRoutes);
 app.use("/api/references", referenceRoutes);
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://refhub.site'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false,
+  maxAge: 86400
+}));
 
 app.get('/aws', (req, res) => {
   res.status(200).send('OK');
