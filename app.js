1c346226df6c09a2ec7dac5e9a510f9d6d4b38bc
SONG <dlthdal07@gmail.com>
2025-02-03 20:52:12 +0900

    Feat: CORS 설정 도메인 추가

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'https://refhub.site',
-    'https://api.refhub.site'
+    'https://api.refhub.site',
+    'https://www.refhub.my',
+    'https://refhub.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
