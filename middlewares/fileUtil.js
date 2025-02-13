64b0176038a4863af218eea4342dd23335c42297
Minhee Han <96728777+KwakSsi38@users.noreply.github.com>
2025-02-13 12:11:54 +0900

    Chore: BASE_URL 하드코딩

--- middlewares/fileUtil.js
+++ middlewares/fileUtil.js
@@ -3,5 +3,5 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const getFileUrl = (fileId) => {
-    return `${process.env.BASE_URL}/api/references/file/${fileId}`;
+    return `https://refhub.my/api/references/file/${fileId}`;
   };
