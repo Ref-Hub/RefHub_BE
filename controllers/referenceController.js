6501eee1c1b49787014634c177197ce4d7d48f74
ljm1102 <127467536+ljm1102@users.noreply.github.com>
2025-03-15 20:48:19 +0900

    레퍼런스 수정 : 이미지 리스트 삭제 오류 테스트

--- controllers/referenceController.js
+++ controllers/referenceController.js
@@ -263,6 +263,7 @@ export const updateReference = async (req, res) => {
       bucketName: "uploads",
     });
 
+    /*
     // 기존 첨부 자료 삭제
     for (const file of reference.files) {
       console.log("파일 데이터:", file); // 각 file 객체 출력
@@ -303,6 +304,7 @@ export const updateReference = async (req, res) => {
         }
       }
     }
+    */
 
     // 기존 첨부 자료 초기화
     const files = [];
