256d2ad572c4717b70e5d2341d51eb154176790d
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-03-21 16:32:39 +0900

    Feat: Collction res에 공유 관련 필드 3개 추가

--- models/Collection.js
+++ models/Collection.js
@@ -14,8 +14,19 @@ const collSchema = new mongoose.Schema(
     },
     createdAt: { type: Date, default: () => Date.now() },
   },
-  { versionKey: false }
+  {
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
+
+collSchema.virtual("share", {
+  ref: "CollectionShare",
+  localField: "_id",
+  foreignField: "collectionId",
+  justOne: true,
+});
 
 // Index 설정 (createdBy와 title의 조합을 유니크로 설정)
 collSchema.index({ createdBy: 1, title: 1 }, { unique: true });
