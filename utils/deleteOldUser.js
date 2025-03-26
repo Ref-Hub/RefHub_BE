9d6ed9910ff86eca3fd333fe8000b6b38cc855d0
SONG <dlthdal07@gmail.com>
2025-03-26 23:43:36 +0900

    Chore: 계정 자동 삭제 DB 연결 수정

--- utils/deleteOldUser.js
+++ utils/deleteOldUser.js
@@ -1,25 +1,31 @@
-import User from '../models/User.js';
+import mongoose from "mongoose";
+import User from "../models/User.js";
+import dotenv from "dotenv";
+import connectDB from "../db.js";
+
+dotenv.config();
 
 const deleteOldUsers = async () => {
-  console.log('🛠️ 7일이 지난 탈퇴 요청 계정을 삭제하는 작업을 시작합니다.');
+  console.log("🛠️ 7일이 지난 탈퇴 요청 계정을 삭제하는 작업을 시작합니다.");
+
+  await connectDB();  
 
   const sevenDaysAgo = new Date();
   sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
 
   try {
     // 삭제 대상 계정 조회
-    const usersToDelete = await User.find({ 
+    const usersToDelete = await User.find({
       deleteRequestDate: { $lte: sevenDaysAgo }
     });
 
-    // 삭제 대상 계정 정보 조회
-    console.log('🛠️ 삭제된 계정 목록:');
+    console.log("🛠️ 삭제된 계정 목록:");
     usersToDelete.forEach(user => {
       console.log(`이름: ${user.name}, 이메일: ${user.email}`);
     });
 
     // 계정 삭제
-    const deletedUsers = await User.deleteMany({ 
+    const deletedUsers = await User.deleteMany({
       deleteRequestDate: { $lte: sevenDaysAgo }
     });
 
@@ -27,6 +33,7 @@ const deleteOldUsers = async () => {
   } catch (error) {
     console.error('7일이 지난 탈퇴 요청 계정 삭제 중 오류가 발생했습니다.:', error);
   } finally {
+    await mongoose.disconnect();
     process.exit(0);
   }
 };
