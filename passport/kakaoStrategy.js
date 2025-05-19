2902b04ee65681e9ae3bfa683272869a14fe06ec
SONG <dlthdal07@gmail.com>
2025-05-19 23:39:18 +0900

    Feat: kakaoStrategy 로직 개선

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -11,28 +11,39 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       try {
         const kakaoAccount = profile._json.kakao_account;
-        console.log('카카오 로그인 이메일:', kakaoAccount.email);
+        const kakaoEmail = kakaoAccount?.email;
 
-        if (!kakaoAccount.email) {
-          console.error('이메일 없음');
-          return done(new Error('이메일 누락'), null);
+        console.log('카카오 로그인 이메일:', kakaoEmail);
+
+        if (!kakaoEmail) {
+          console.error('이메일 정보가 없습니다.');
+          return done(new Error('카카오 계정에 이메일이 없습니다.'), null);
         }
 
-        let user = await User.findOne({ email: kakaoAccount.email });
+        let user = await User.findOne({ email: kakaoEmail });
+
+        // if (user && user.provider !== 'kakao') {
+        //   console.error('이미 이메일로 가입된 유저입니다.');
+        //   return done(new Error('이미 가입된 이메일입니다. 이메일 로그인을 이용하세요.'), null);
+        // }
 
         if (!user) {
           user = await User.create({
             name: profile.displayName,
-            email: kakaoAccount.email,
+            email: kakaoEmail,
             password: '',
-            profileImage: kakaoAccount.profile?.profile_image_url,
+            profileImage: kakaoAccount.profile?.profile_image_url || '',
             provider: 'kakao',
           });
+
+          console.log('신규 유저 생성 완료:', user.email);
+        } else {
+          console.log('기존 유저 로그인:', user.email);
         }
 
         return done(null, user);
       } catch (err) {
-        console.error('카카오 전략 내부 에러:', err);
+        console.error('카카오 전략 내부 에러:', err.message);
         return done(err, null);
       }
     }
